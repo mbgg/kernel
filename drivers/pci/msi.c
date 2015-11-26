6afbaada15cee1720559bbc46e72f3a8b9174f59
Takashi Iwai <tiwai@suse.de>
2015-11-26 15:13:41 +0100

    Fix kABI breakage in drivers/pci/msi.c (boo#956856).
    suse-commit: cae56ba1b229f0842003d9457d2385ead2777242

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -20,7 +20,6 @@
 #include <linux/io.h>
 #include <linux/slab.h>
 #include <linux/irqdomain.h>
-#include <linux/of_irq.h>
 
 #include "pci.h"
 
@@ -1363,6 +1362,8 @@ struct irq_domain *pci_msi_create_default_irq_domain(struct device_node *node,
 
 	return domain;
 }
+
+#include <linux/of_irq.h>
 
 static int get_msi_id_cb(struct pci_dev *pdev, u16 alias, void *data)
 {
